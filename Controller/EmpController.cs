89a21f361702f9b02b5dc88f8b0e8f3928a15ab7
Negasi <negasig21@gmail.com>
2026-06-01 23:15:14 +0300

    initial commit

---
+++ Controller/EmpController.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerizedApp.Controller
+{
+    [ApiController]
+    [Route("api")]
+
+    public class EmpController:ControllerBase
+    {
+        [HttpGet("test")]
+        
+        public ActionResult<string> Test()
+        {
+             return Ok("Test Api Docker");
+        }
+
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,11 @@
+﻿FROM mcr.microsoft.com/dotnet/sdk:latest AS build
+WORKDIR /app
+COPY *.csproj ./
+RUN dotnet restore
+COPY . ./
+RUN dotnet publish -c Release -o out
+FROM mcr.microsoft.com/dotnet/aspnet:latest AS runtime
+WORKDIR /app
+COPY --from=build /app/out ./
+EXPOSE 8058
+CMD ["dotnet", "DockerizedApp.dll"]
